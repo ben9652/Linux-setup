f24fad8336a251d800989ea529059851c7ad017d
Benjamín Boeri <ben9652@gmail.com>
2026-09-19 16:40:18 -0300

    Añado configs de nvim

---
+++ nvim/lua/plugins/teamtype.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "teamtype/teamtype-nvim",
+    lazy = false,
+    keys = {
+      { "<leader>ej", "<cmd>TeamtypeJumpToCursor<cr>", desc = "Teamtype: ir al cursor remoto" },
+      { "<leader>ef", "<cmd>TeamtypeFollow<cr>", desc = "Teamtype: seguir cursor remoto" },
+    },
+  },
+}
